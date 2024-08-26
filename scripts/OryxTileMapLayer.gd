afd01a8819d253c7b4e4ef22e115cc8f2228a51a
Sam Judd <samjudd@protonmail.com>
2024-08-25 23:00:54 -0700

    add base level class, make instance of it for dev level, make that level main

--- scripts/OryxTileMapLayer.gd
+++ scripts/OryxTileMapLayer.gd
@@ -9,5 +9,8 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
+	pass
+
+func astar_setup() -> void:
 	pass
